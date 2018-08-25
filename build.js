e81fb2c137fdf07e38c6c9fb330d432bb2a6b8f4
Matthew Lloyd <Matt.Lloyd101@gmail.com>
2018-08-25 20:11:29 +0100

    Still initial setup

---
+++ build.js
@@ -0,0 +1,9 @@
+({
+    baseUrl: "src/js",
+    name: "app",
+    out: "out/js/app.js",
+    paths: {
+       text: "../../node_modules/text/text",
+       glMatrix: "../../lib/glMatrix-0.9.5.min"
+    }
+})
